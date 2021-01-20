88245ed06c0f84ed95ca8dd17e64f210293e6533
Arianna Kellogg <ari@truss.works>
2021-01-20 15:39:22 -0800

    rendering table with hard coded text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>The Planets of Star Wars</h1>
+        <table>
+          <td>
+            <th>planet</th>
+            <td>climate</td>
+            <td>number of residents</td>
+            <td>terrains</td>
+            <td>population</td>
+            <td>surface area covered by water</td>
+          </td>
+        </table>
       </header>
+      <footer className="App-footer">
+        <p>TrussWorks, Inc. FE work sample, January 2021</p>
+      </footer>
     </div>
   );
 }
